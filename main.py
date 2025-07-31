0fddfe631de40998c1819b0085a5fd4e2e8db911
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-07-31 22:13:19 +0300

    add helicopter

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+from helicopter import Helicopter as Helico
 from map import Map
 import time
 import os
 
 TICK_SLEEP = 0.09
 TREE_UPDATE = 5
+FIRE_UPDATE = 10
+MAP_W, MAP_H = 20,10
 
-tmp = Map(20, 10)
+tmp = Map(MAP_W, MAP_H)
 tmp.generate_forest(3, 10)
 tmp.generate_river(10)
 tmp.generate_river(10)
 tmp.add_fire()
 tmp.print_map()
 
+helico = Helico(MAP_W, MAP_H)
+
 tick = 1
 while True:
     os.system("cls")
     print("TICK", tick)
-    tmp.print_map()
+    tmp.print_map(helico)
     tick += 1
     time.sleep(TICK_SLEEP)
     if (tick % TREE_UPDATE == 0):
         tmp.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        tmp.update_fires()
--- map.py
+++ map.py
@@ -11,6 +11,28 @@ CELL_TYPES = "⬜🌲🌊🚑🚀🔥"
 
 
 class Map:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells = [[0 for i in range(w)] for j in range(h)]
+
+    def check_bounds(self, x, y):
+        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
+            return False
+        return True
+    def print_map(self, helico):
+        print("🏁" * (self.w + 2))
+        for ri in range(self.h):
+            print("🏁", end="")
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if (helico.x == ri and helico.y == ci):
+                    print("🚁", end="")
+                elif (cell >= 0 and cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell], end="")
+            print("🏁")
+        print("🏁" * (self.w + 2))
+
     def generate_river(self, l):
         rc = randcell(self.w, self.h)
         rx, ry = rc[0], rc[1]
@@ -22,41 +44,28 @@ class Map:
                 self.cells[ry2][rx2] = 2
                 rx, ry = rx2, ry2
                 l -= 1
-
     def generate_forest(self, r, mxr):
         for ri in range(self.h):
             for ci in range(self.w):
                 if randbool(r, mxr):
                     self.cells[ri][ci] = 1
-
     def generate_tree(self):
         c = randcell(self.w, self.h)
         cx, cy = c[0], c[1]
         if (self.check_bounds(cx, cy) and self.cells[cy][cx] == 0):
             self.cells[cy][cx] = 1
 
-    def print_map(self):
-        print("🏁" * (self.w + 2))
-        for row in self.cells:
-            print("🏁", end="")
-            for cell in row:
-                if (cell >= 0 and cell < len(CELL_TYPES)):
-                    print(CELL_TYPES[cell], end="")
-            print("🏁")
-        print("🏁" * (self.w + 2))
 
     def add_fire(self):
         c = randcell(self.w, self.h)
         cx, cy = c[0], c[1]
         if self.cells[cy][cx] == 1:
             self.cells[cy][cx] = 5
-
-    def check_bounds(self, x, y):
-        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
-            return False
-        return True
-
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        self.cells = [[0 for i in range(w)] for j in range(h)]
+    def update_fires(self):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+        for i in range(5):
+            self.add_fire()
